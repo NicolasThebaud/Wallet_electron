f402a41e8a44b08f75bac1fd5419ff177d50be49
NicolasThebaud <thebaud.nicolas012@gmail.com>
2016-06-12 16:29:02 +0200

    React has entered the game

--- app.js
+++ app.js
@@ -3,4 +3,19 @@
 
 	require('jquery')
 	require('materialize-css')
+	require('./components/build')
+
+	const fs = require('fs')
+		ReactDOM = require('react-dom')
+
+	ReactDOM.render(< Balance / >, document.getElementById('lines'))
+
+	/*fs.readFile('dataset.json', (err,buff)=>{
+		if(err) throw err
+		let data = JSON.parse(buff.toString()),
+			lines = data.lines
+		for(let line of lines) {
+
+		}
+	})*/
 })()
---
+++ components/balance.js
@@ -0,0 +1,16 @@
+class Balance extends React.Component {
+	render() { return (
+			<div>
+				<span class="line-title"></span>
+				<span class="line-balance"></span>
+			</div>
+			<div>
+				<div class="row">
+					<div class="col s10"><p class="line-title"></p></div>
+					<div class="col s2"><p class="p-padding"></p></div>
+				</div>
+			</div>
+	)}
+}
+
+export default Balance
